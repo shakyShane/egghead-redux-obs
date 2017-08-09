cceb937e554014d52c9e81293e1e3ab7c2b0c4cf
Shane Osbourne <shane.osbourne8@gmail.com>
2017-08-09 14:41:19 +0100

    Add Redux to a React application

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  items: [],
+};
+
+export function storiesReducer(state = initialState, action) {
+  return state;
+}
+
+export default storiesReducer;
